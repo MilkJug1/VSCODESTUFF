072ce24a21fb065c56bac673f64105d376213c16
waterbottle-123 <michael.sagan.crawford@gmail.com>
2021-02-27 20:38:01 -0500

    cpp

--- cpplearning.cpp
+++ cpplearning.cpp
@@ -8,9 +8,22 @@
 #include <ctime>
 #include <cmath>
 
+int imGlobal = 0;
+const double PI = 69;
 
 int main(int argc, char ** argv) 
 {
-    std::cout << "hello\n ";
+    std::cout << PI << std::endl;
+
+    double tempf = 51;
+    double tempc;
+
+    tempc(tempf - 32)/1.8;
+
+
+    bool married = true;
+    
+
+    return 0;
 }
 
